51523f4130e6e2bdbb23b74330507ae635febb53
Ekta Chaudhry <ekta.chaudhry0701@gmail.com>
2024-07-11 17:47:25 +0000

    Added number basics

---
+++ 01_basics/numbersAndMaths.js
@@ -0,0 +1,19 @@
+const score = 400
+//console.log(score)
+
+//explicit declaration of Number type
+const balance = new Number(400)
+//console.log(balance)
+//score is of type number while balance is of type object
+//console.log(typeof score)
+//console.log(typeof balance)
+
+//balance gets converted to type string
+//console.log(typeof balance.toString())
+
+//methods on a number object
+//console.log(balance.toFixed(2))
+
+const bigNumber = 100000000
+//console.log(bigNumber.toLocaleString('en-IN'))
+
