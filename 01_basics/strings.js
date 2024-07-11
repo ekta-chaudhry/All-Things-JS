7b2310c96f498571ba38578fac3ad11c0621fa3c
Ekta Chaudhry <ekta.chaudhry0701@gmail.com>
2024-07-11 04:40:59 +0000

    strings basics

---
+++ 01_basics/strings.js
@@ -0,0 +1,7 @@
+const gameName = "Ektaec"
+
+console.log(`The value of gameName is ${gameName}`)
+console.log(gameName[0])
+console.log(gameName.__proto__)
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
