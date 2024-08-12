71a4e347dd9f4108b809efacbd01662e84e25633
Ekta Chaudhry <ekta.chaudhry0701@gmail.com>
2024-08-12 13:39:01 +0000

    Added more points - in-depth

--- 01_basics/mixedDatatypesOperations.js
+++ 01_basics/mixedDatatypesOperations.js
@@ -1,10 +1,16 @@
 let value = 3;
-let negValue = -value;
+let negValue = -value
 console.log(negValue)
 
 let str1 = "Hello"
 let str2 = "World"
-let str3 = str1 + str2
+let str3 = str1 + str2 //adding two string values - yields another string
 console.log(str3)
 
-console.log("3"+ 48 + 1)
+console.log("3"+ 48 + 1)  //=> 3481; when numeric values and string values are used with + operator,
+// numeric value gets converted to a string.
+console.log(3 + 48 + "1") //=> 511; the expression is evaluated from left to right, so 3 + 48 = 51
+//but 51 + "1" yields 511 due to conversion of 51 to a string value.
+console.log("3" * 9)  //=> 27; conversion happens from strin to numeric value only when the operator is not +
+console.log("3abc" * 9) //=>NaN; the string value doesn't respresent a valid numeric value, so the operation
+//yields a NaN.
--- 01_basics/strings.js
+++ 01_basics/strings.js
@@ -1,7 +1,14 @@
-const gameName = "Ektaec"
+let gameName = new String("Ektaec")
 
 console.log(`The value of gameName is ${gameName}`)
 console.log(gameName[0])
 console.log(gameName.__proto__)
 console.log(gameName.length)
 console.log(gameName.toUpperCase())
+
+let another = gameName
+another = "Another"
+console.log(`Another: ${another} and gameName: ${gameName}`)
+another[0] = 'b'
+console.log(`Another: ${another} and gameName: ${gameName}`)
+
